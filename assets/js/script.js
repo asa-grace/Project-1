d06eae7d05b4f12aae48aa60583c86f6d5cd73bb
Biznaz <aldhelm7@gmail.com>
2021-06-07 15:45:29 -0700

    added clear button and multiple saved quote functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,15 @@ function famousQuotes() {
       var selectedQuote = response.data[indexSelect].quoteText + '-' +  response.data[indexSelect].quoteAuthor;
       console.log(selectedQuote);
 
-      var generatedQuote = document.querySelector('#generatedQuote');
+      var generatedQuote = document.querySelector('#quote-text');
       generatedQuote.innerHTML = selectedQuote;
 
       document.querySelector('#saveBtn').addEventListener("click", function() {
-        localStorage.setItem("savedQuote", selectedQuote);
-        var quoteStorage = localStorage.getItem('savedQuote');
-        console.log(quoteStorage);
-        savedEl = document.querySelector('#quoteStorage');
-        savedEl.innerHTML = quoteStorage;
+        localStorage.setItem("famousQuote" + i, selectedQuote);
+        //var quoteStorage = localStorage.getItem('famousQuote');
+        //console.log(quoteStorage);
+        //savedEl = document.querySelector('#quoteStorage');
+        //savedEl.innerHTML = quoteStorage;
     })
   });
 }
@@ -33,17 +33,17 @@ function gameOfThrones() {
         return response.json();
       })
       .then(function(response) {
-        console.log(response.sentence + '-' + response.character.name)
+        //console.log(response.sentence + '-' + response.character.name)
         var selectedQuote = response.sentence + '-' + response.character.name;
-        var generatedQuote = document.querySelector('#generatedQuote');
+        var generatedQuote = document.querySelector('#quote-text');
         generatedQuote.innerHTML = selectedQuote;
 
         document.querySelector('#saveBtn').addEventListener("click", function() {
-          localStorage.setItem("savedQuote", selectedQuote);
-          var quoteStorage = localStorage.getItem('savedQuote');
-          console.log(quoteStorage);
-          savedEl = document.querySelector('#quoteStorage');
-          savedEl.innerHTML = quoteStorage;
+          localStorage.setItem("gotQuote" + i, selectedQuote);
+          //var quoteStorage = localStorage.getItem('gotQuote' + i);
+          //console.log(quoteStorage);
+          //savedEl = document.querySelector('#quoteStorage');
+          //savedEl.innerHTML = quoteStorage;
     }) 
   });
 }
@@ -52,17 +52,18 @@ function randomQuotes () {
   fetch('https://api.quotable.io/random')
   .then(response => response.json())
   .then(data => {
-    console.log(`${data.content} —${data.author}`)
+    //console.log(`${data.content} —${data.author}`)
     var selectedQuote = `${data.content} —${data.author}`;
-    var generatedQuote = document.querySelector('#generatedQuote');
+    var generatedQuote = document.querySelector('#quote-text');
     generatedQuote.innerHTML = selectedQuote;
 
+
     document.querySelector('#saveBtn').addEventListener("click", function() {
-      localStorage.setItem("savedQuote", selectedQuote);
-      var quoteStorage = localStorage.getItem('savedQuote');
-      console.log(quoteStorage);
-      savedEl = document.querySelector('#quoteStorage');
-      savedEl.innerHTML = quoteStorage;
+      localStorage.setItem("randomQuote" + i, selectedQuote);
+      //var quoteStorage = localStorage.getItem('randomQuote' + i);
+      //console.log(quoteStorage);
+      //savedEl = document.querySelector('#quoteStorage');
+      //savedEl.innerHTML = quoteStorage;
     }) 
   })  
 }
@@ -85,5 +86,38 @@ function randomGif() {
       gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
 
       responseContainerE1.appendChild(gifImg);
+
     });
+  }
+
+  var storedQuotes = [];
+
+  function allStorage() {
+    keys= Object.keys(localStorage),
+    i=keys.length;
+
+    while (i--) {
+      storedQuotes.push(localStorage.getItem(keys[i]))
+    }
+    console.log(storedQuotes);
+    return storedQuotes;
+  }
+  allStorage();
+  
+  var quoteStorageDiv = document.getElementById('quoteStorage');
+
+  for (i = 0; i < storedQuotes.length; i++) {
+    var createPara = document.createElement('p');
+    createPara.setAttribute('id', 'savedQuoteP');
+    createPara.textContent = storedQuotes[i];
+    quoteStorageDiv.appendChild(createPara);
+  };
+  
+
+  //clear local storage
+  function clearSavedQuotes() {
+    var savedQuoteEl = document.querySelector('#quoteStorage');
+    savedQuoteEl.textContent = '';
+    localStorage.clear();
+
   }
